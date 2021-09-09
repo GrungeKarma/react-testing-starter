5242054af79c9c2fe3f2c2cc85efe11e236f9ade
Anthony Lopez <grungekarma@gmail.com>
2021-09-09 16:01:24 -0500

    add unit tests & end to end test

---
+++ src/components/TransactionCreateStepTwo.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import TransactionCreateStepTwo from "./TransactionCreateStepTwo";
+import userEvent from "@testing-library/user-event";
+
+test("on initial render, the pay button is disabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeDisabled();
+});
+
+test("if an amount is entered, the pay button is enabled", async () => {
+  render(<TransactionCreateStepTwo sender={{ id: "5" }} receiver={{ id: "5" }} />);
+  userEvent.type(screen.getByPlaceholderText(/amount/i), "50");
+  userEvent.type(screen.getByPlaceholderText(/add a note/i), "dinner");
+
+  expect(await screen.findByRole("button", { name: /pay/i })).toBeEnabled();
+});
